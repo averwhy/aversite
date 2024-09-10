45767c45e386312e35ce5d24cd09119f4e0953a0
averyB <averwhy@gmail.com>
2024-09-10 14:28:21 -0400

    Trying to get tailwind to work

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import viteReact from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,9 @@ export default defineConfig({
     TanStackRouterVite(),
     viteReact()
   ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  }
 });
