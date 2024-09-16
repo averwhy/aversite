6c78a58c969a646f668007d33c2d5e0b24e3af27
avery b <averwhy@gmail.com>
2024-09-16 01:14:40 -0400

    Gallery testing

--- src/routes/gallery.lazy.tsx
+++ src/routes/gallery.lazy.tsx
@@ -1,13 +1,43 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
+import Imgix from "react-imgix";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#1e293b',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center'
+}));
 
 export const Route = createLazyFileRoute('/gallery')({
   component: Gallery,
 })
 
+function lotsOfPics(){
+  var picList = [];
+  for (let i = 0; i < 50; i++){
+    picList.push(
+      <Grid size="auto">
+        <Item>
+          <Imgix src="https://scmapdb.wdfiles.com/local--resized-images/wad:developer-textures/dev_256_or_032x.jpg/small.jpg"></Imgix>
+        </Item>
+      </Grid>
+    )
+  }
+  return picList;
+}
+
 function Gallery() {
   return (
     <div className="p-2 text-white">
-      Hello from Gallery!
+      <Box sx={{flexGrow: 1}}>
+        <Grid container spacing={2}>
+          {lotsOfPics()}
+        </Grid>
+      </Box>
     </div>
   )
 }
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,5 +1,4 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
-import { Planet } from "react-planet"
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
@@ -8,12 +7,8 @@ export const Route = createLazyFileRoute('/')({
 function Index() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div>
-        <Planet centerContent={<div style={{height: 100, width: 100, borderRadius: '50%', backgroundColor: '#1da8a4'}} />} open autoClose
-          > <div style={{height: 70, width: 70, borderRadius: '50%', backgroundColor: '#9257ad'}}/>
-          <div style={{height: 70, width: 70, borderRadius: '50%', backgroundColor: '#9257ad'}}/>
-          <div style={{height: 70, width: 70, borderRadius: '50%', backgroundColor: '#9257ad'}}/>
-        </Planet>
+      <div className="text-white font-bold">
+        hey
       </div>
     </div>
   )
