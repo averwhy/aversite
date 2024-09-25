cc4995e29e2b6c67c3c14b278ef3cf7a3232feb1
avery b <averwhy@gmail.com>
2024-09-25 14:49:38 -0400

    primereact works yippee

--- src/routes/portfolio.lazy.tsx
+++ src/routes/portfolio.lazy.tsx
@@ -8,8 +8,8 @@ export const Route = createLazyFileRoute('/portfolio')({
 function Portfolio() {
   return (
     <div className='h-screen p-32 items-center justify-center'>
-      <Card title="Simple Card" subTitle="Python, discord.py, PostgreSQL" className='text-white font-bold'>
-        <p className="m-0 font-normal">
+      <Card title="Simple Card" subTitle="Python, discord.py, PostgreSQL" className='bg-slate-700 text-white font-bold md:w-96 sm:w-56'>
+        <p className="font-normal">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
             numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
         </p>
