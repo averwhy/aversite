d86e1979d35089b297237070160cbde7720b8144
averyB <averwhy@gmail.com>
2024-10-02 03:54:46 -0400

    huge massive progress!

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -6,9 +6,14 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="text-white font-bold">
+    <div className="flex h-screen items-center justify-center">
+      <div className="font-bold text-white">
         hey
+        <p className='pt-2 pb-10 text-center text-gray-400'>
+          this site is a work in progress! so far /portfolio has had the most work done on it
+          <br/>
+          thanks for visiting :D
+        </p>
       </div>
     </div>
   )
--- src/routes/portfolio.lazy.tsx
+++ src/routes/portfolio.lazy.tsx
@@ -8,13 +8,22 @@ export const Route = createLazyFileRoute('/portfolio')({
 
 const ecoxGithubURL = "https://github.com/averwhy/EconomyX"
 const ecoxInviteURL = "https://discord.com/api/oauth2/authorize?client_id=780480654277476352&permissions=0&scope=bot"
-const ecoxSupportURL = ""
+const ecoxSupportURL = "https://discord.gg/epQZEp933x"
 
-const ecoCardPT = {
+const fishyGithubURL = "https://github.com/averwhy/fishypy-discord-game"
+const fishyInviteURL = "https://discord.com/api/oauth2/authorize?client_id=708428058822180874&permissions=289856&scope=bot"
+const fishySupportURL = "https://discord.gg/HSqevex"
+
+const timSourceURL = "https://github.com/averwhy/TimBot"
+const timServerURL = "https://discord.gg/snhuesports"
+
+const aversiteSourceURL = "https://github.com/averwhy/aversite"
+
+const basicCardPT = {
   subTitle: { className: 'text-slate-300 font-light text-sm m-0 mb-1' },
   title : { className: 'mb-0' },
   content: { className: 'py-1' },
-  footer: { className: 'py-2' },
+  footer: { className: 'py-2 flex gap-5 place-content-center' },
 }
 
 const ecoxHeader = (
@@ -23,58 +32,103 @@ const ecoxHeader = (
 const fpyHeader = (
   <img alt='Fishy.py banner' src="./../fishypy banner.png" height={220} width={518}/>
 )
+const timHeader = (
+  <img alt='TimBot banner' src="./../snhuesports.png" height={220} width={518}/>
+)
 
 function ecoSC(){ window.open(ecoxGithubURL) }
 function ecoIC(){ window.open(ecoxInviteURL) }
 function ecoTIC(){ window.open(ecoxSupportURL) }
 
+function fishySC(){ window.open(fishyGithubURL) }
+function fishyIC(){ window.open(fishyInviteURL) }
+function fishyTIC(){ window.open(fishySupportURL) }
+
+function timSC(){ window.open(timSourceURL) }
+function timTIC(){ window.open(timServerURL) }
+
+function aversiteSC(){ window.open(aversiteSourceURL) }
+
 const ecoxFooter = (
   <>
-    <Button label="Source" icon="pi pi-github" link onClick={ecoSC} className='bg-slate-900 p-3 hover:bg-slate-950'/>
+    <Button label="Source" icon="pi pi-github" link onClick={ecoSC} className='text-pretty bg-slate-900 p-3 hover:bg-slate-950'/>
     {" "}
-    <Button label="Invite" icon="pi pi-discord" link onClick={ecoIC} className='bg-blurple p-3 hover:bg-new-blurple'/>
+    <Button label="Invite" icon="pi pi-discord" link onClick={ecoIC} className='text-pretty bg-blurple p-3 hover:bg-new-blurple'/>
     {" "}
-    <Button label="Try it" icon="pi pi-comments" link onClick={ecoTIC} className='bg-slate-800 p-3 hover:bg-slate-900'/>
+    <Button label="Try it" icon="pi pi-comments" link onClick={ecoTIC} className='text-pretty bg-slate-800 p-3 hover:bg-slate-900'/>
+  </>
+)
+
+const fishyFooter = (
+  <>
+    <Button label="Source" icon="pi pi-github" link onClick={fishySC} className='text-pretty bg-slate-900 p-3 hover:bg-slate-950'/>
+    {" "}
+    <Button label="Invite" icon="pi pi-discord" link onClick={fishyIC} className='text-pretty bg-blurple p-3 hover:bg-new-blurple'/>
+    {" "}
+    <Button label="Try it" icon="pi pi-comments" link onClick={fishyTIC} className='text-pretty bg-slate-800 p-3 hover:bg-slate-900'/>
+  </>
+)
+
+const timFooter = (
+  <>
+    <Button label="Source" icon="pi pi-github" link onClick={timSC} className='text-pretty bg-slate-900 p-3 hover:bg-slate-950'/>
+    {" "}
+    <Button label="Try it" icon="pi pi-comments" link onClick={timTIC} className='text-pretty bg-slate-800 p-3 hover:bg-slate-900'/>
+  </>
+)
+
+const aversiteFooter = (
+  <>
+    <Button label="Source" icon="pi pi-github" link onClick={aversiteSC} className='text-pretty bg-slate-900 p-3 hover:bg-slate-950'/>
   </>
 )
 
 const ecoCardTitle = ( <>EconomyX <span className="pi pi-discord" /></> )
+const timCardTitle = ( <>TimBot <span className="pi pi-discord" /></> )
 const fishCardTitle = ( <>fishy.py <span className="pi pi-discord" /></> )
 
 function Portfolio() {
   return (
-    <div className="flex h-screen animate-fade items-center justify-center p-16">
-      <div className="grid grid-cols-2 place-items-start gap-3 lg:grid-cols-3 lg:gap-32">
-        <Card header={fpyHeader} title={fishCardTitle} subTitle="python, discord.py, sqlite" footer={ecoxFooter} pt={ecoCardPT} className="bg-slate-700 font-bold font-mono text-white sm:w-56 md:w-96">
-          <p className="font-normal text-sm xs:text-xs">
-              Fishy.py is recreation of a old Discord bot, which was a reaction-based fishing game with over 16,000 catchable fish.
-              This was my first major bot project that taught me a lot about Python, SQLite and common coding practices.
-              Fishy.py is in 320+ servers, and while not as successful as some of my other projects, it was still a project i throughly enjoyed making.
-          </p>
-        </Card>
-        <Card header={ecoxHeader} title={ecoCardTitle} subTitle="python, discord.py, postgreSQL" footer={ecoxFooter} pt={ecoCardPT} className="bg-slate-700 font-bold font-mono text-white sm:w-56 md:w-96">
-          <p className="font-normal">
-              EconomyX is a unique & simple economy game bot for Discord. This project began in 2020, starting out with just a few commands and a lightweight
-              SQLite database.
-              Today, it's come to have 6+ gambling commands <span className="text-slate-300 text-xs italic">(remember it's a virtual currency!)</span>,
-              a stocks system, lottery, jobs system, and a treasure digging sim coming soon.
-          </p>
-          <p>
-            EconomyX is in 855+ servers, with 1,080+ players.
-          </p>
-        </Card>
-        <Card header={ecoxHeader} title={ecoCardTitle} subTitle="python, discord.py, postgreSQL" footer={ecoxFooter} pt={ecoCardPT} className="bg-slate-700 font-bold font-mono text-white sm:w-56 md:w-96">
-          <p className="font-normal">
-              EconomyX is a unique & simple economy game bot for Discord. This project began in 2020, starting out with just a few commands and a lightweight
-              SQLite database.
-              Today, it's come to have 6+ gambling commands <span className="text-slate-300 text-xs italic">(remember it's a virtual currency!)</span>,
-              a stocks system, lottery, jobs system, and a treasure digging sim coming soon.
-          </p>
-          <p>
-            EconomyX is in 855+ servers, with 1,080+ players.
-          </p>
-        </Card>
+    <>
+      <p className="pt-5 text-center text-6xl text-white tracking-widest">portfolio</p>
+      <p className="pt-2 pb-10 text-center text-gray-400">coding projects, organization work & more</p>
+      <div className="flex animate-fade items-center justify-center p-1">
+        <div className="grid grid-cols-1 place-items-start gap-3 gap-y-10 lg:grid-cols-2 xl:grid-cols-3 xl:gap-32">
+          <Card header={fpyHeader} title={fishCardTitle} subTitle="python, discord.py, sqlite" footer={fishyFooter} pt={basicCardPT} className="bg-slate-700 font-bold font-mono text-white sm:w-56 md:w-96">
+            <p className="font-normal text-xs lg:text-sm">
+                Fishy.py is recreation and revival of the old Discord bot Fishy, which was a reaction-based fishing game with over 16,000 catchable fish.
+                This was my first major bot project that taught me a lot about Python, SQLite and common coding practices.
+                While not as successful as some of my other projects, it was still a project i throughly enjoyed making, as well as learned a lot through.
+            </p>
+            <p>Fishy.py is in 320+ servers, with 600+ unique players.</p>
+          </Card>
+          <Card header={ecoxHeader} title={ecoCardTitle} subTitle="python, discord.py, postgreSQL" footer={ecoxFooter} pt={basicCardPT} className="bg-slate-700 font-bold font-mono text-white sm:w-56 md:w-96">
+            <p className="font-normal text-xs lg:text-sm">
+                EconomyX is a unique & simple economy game bot for Discord. This project began in 2020, starting out with just a few commands and a lightweight
+                SQLite database.
+                Today, it's come to have 6+ gambling commands <span className="text-slate-300 text-xs italic">(remember it's a virtual currency!)</span>,
+                a stocks system, lottery, jobs system, and a treasure digging sim coming soon.
+            </p>
+            <p>
+              EconomyX is in 855+ servers, with 1,080+ players.
+            </p>
+          </Card>
+          <Card header={timHeader} title={timCardTitle} subTitle="python, discord.py, postgreSQL" footer={timFooter} pt={basicCardPT} className="bg-slate-700 font-bold font-mono text-white sm:w-56 md:w-96">
+            <p className="font-normal text-xs lg:text-sm">
+              TimBot is the main utility and moderation bot for the SNHU Esports Discord server.
+              <span className="text-slate-300 text-xs italic"><br/>If you want to read more about my involvement with SNHU Esports, check out the /about page.<br/></span>
+              TimBot was initally made as a ticket managing bot, so 2,000+ users could seek support for recruitment, tech issues with our Arena, or general program questions if they needed to. However over the time it's been
+              a bot for the server, multiple features have been added such as 'reaction' roles, GGCircuit integration, and more.
+            </p>
+          </Card>
+          <Card header={timHeader} title="This site!" subTitle="react, tailwind css, vite, biome + more" footer={aversiteFooter} pt={basicCardPT} className="bg-slate-700 font-bold font-mono text-white sm:w-56 md:w-96">
+            <p className="font-normal text-xs lg:text-sm">
+              I hand coded this website that you're reading today! It was much needed and i think it came out looking pretty nice :)
+            </p>
+          </Card>
+        </div>
       </div>
-    </div>
+      <br/>
+    </>
   )
 }
