b29e1bba46d078dca67df84b10f2335450311cdf
avery b <averwhy@gmail.com>
2024-10-02 17:06:23 -0400

    fine tuning gallery async funcs + add some gallery images

--- src/routes/gallery.lazy.tsx
+++ src/routes/gallery.lazy.tsx
@@ -1,23 +1,42 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { asyncFolderWalker } from 'async-folder-walker';
+import { useEffect, useState } from 'react';
 
 export const Route = createLazyFileRoute('/gallery')({
   component: Gallery,
 })
 
-function lotsOfPics(){
-  var picList = [];
-  for (let i = 0; i < 50; i++){
-    picList.push(
-      <img src="https://scmapdb.wdfiles.com/local--resized-images/wad:developer-textures/dev_256_or_032x.jpg/small.jpg" alt={"Testing pic"}/>
-    )
+const getGalleryPics = async function(): Promise<React.JSX.Element[]>{
+  const walker = asyncFolderWalker('gallery')
+  // biome-ignore lint: shut UP about it being only used once
+  let picList = [];
+  for await (const file of walker) {
+    if (file instanceof String){
+      picList.push(
+        // biome-ignore lint: i promise its okay to put 'image' in the alt text
+        <img src={file.toString()} alt={"Gallery image"}/>
+      )
+    }
   }
   return picList;
 }
 
 function Gallery() {
+  const [galleryPics, setGalleryPics] = useState([]);
+
+  useEffect(() => {
+    const fetchPics = async () => {
+      const pics = await getGalleryPics();
+      setGalleryPics(pics);
+    };
+    fetchPics();
+  }, []);
+
   return (
-    <div className="p-2 text-white">
-          {lotsOfPics()}
+    <div className="flex justify-center p-2 text-white">
+      <div className="grid grid-cols-3 items-center gap-8 gap-x-96 pt-10">
+          {galleryPics}
+      </div>
     </div>
   )
 }
