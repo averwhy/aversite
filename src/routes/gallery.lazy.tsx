d8300bcf5aee08459c544aa2bb7970802f6e99e3
averyB <averwhy@gmail.com>
2024-10-03 03:47:41 -0400

    revert gallery changes because i didnt know i couldnt use fs? lol woops, learning a lot

--- src/routes/gallery.lazy.tsx
+++ src/routes/gallery.lazy.tsx
@@ -6,34 +6,15 @@ export const Route = createLazyFileRoute('/gallery')({
   component: Gallery,
 })
 
-const getGalleryPics = async function(): Promise<React.JSX.Element[]>{
-  const walker = asyncFolderWalker('gallery')
-  // biome-ignore lint: shut UP about it being only used once
-  let picList = [];
-  for await (const file of walker) {
-    if (file instanceof String){
-      picList.push(
-        // biome-ignore lint: i promise its okay to put 'image' in the alt text
-        <img src={file.toString()} alt={"Gallery image"}/>
-      )
-    }
-  }
-  return picList;
-}
+// const getGalleryPics = async function(): Promise<React.JSX.Element[]>{
+  
+// }
 
 function Gallery() {
-  const [galleryPics, setGalleryPics] = useState([]);
-
-  useEffect(() => {
-    const fetchPics = async () => {
-      const pics = await getGalleryPics();
-      setGalleryPics(pics);
-    };
-    fetchPics();
-  }, []);
+  const [galleryPics, setGalleryPics] = useState<React.JSX.Element[]>([]);
 
   return (
-    <div className="flex justify-center p-2 text-white">
+    <div className="flex h-screen justify-center p-2 text-white">
       <div className="grid grid-cols-3 items-center gap-8 gap-x-96 pt-10">
           {galleryPics}
       </div>
