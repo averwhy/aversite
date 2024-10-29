d6939216e8baffa9fa29d8c70a17c1339d430f4f
avery b <averwhy@gmail.com>
2024-10-29 13:46:48 -0400

    gaps and padding on about page

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -7,12 +7,13 @@ export const Route = createLazyFileRoute("/about")({
 function About() {
   return (
     <>
-      <div className="flex animate-fade items-center justify-center p-1">
-        <div className="grid grid-cols-1 place-items-start gap-3 gap-y-5 lg:grid-cols-2 text-white">
-          <img src="https://cdn.averwhy.net/IMG_8388.jpg" />
+      <div className="flex h-screen animate-fade items-center justify-center p-1">
+        <div className="grid grid-cols-1 place-items-start gap-3 gap-x-7 gap-y-7 p-5 text-white lg:grid-cols-2 lg:p-64">
+          <img src="https://cdn.averwhy.net/IMG_8388.jpg" alt="A young couple smiling at the camera"/>
           <p>
-            My name is Avery. I'm a 20 year old aspiring programmer. I'm from New Hampshire, and im currently studying Computer Science Southern New Hampshire University.
-
+            My name is Avery. I'm a 20 year old aspiring programmer. I'm from New Hampshire, and I'm currently studying Computer Science at Southern New Hampshire University.
+            I'm passionate about all things computers, especially programming, hardware, and open source software. I've worked with Python the most, but have also used Javascript/Typescript, 
+            C++, Java, Rust, Lua/Luau, C#, and GDScript (Godot). I love working with databases too, primarily PostgreSQL.
           </p>
           <p>I've always loved technology. Ever since I first started using the computer at 7 years old, I was drawn to it more than anything. I played Minecraft almost every day on my families iMac.</p>
           <p>another pic of some kind</p>
