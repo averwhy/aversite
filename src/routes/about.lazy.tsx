9d3113879f588346308c622735a2e2636a69d43a
averyB <averwhy@gmail.com>
2024-10-29 03:56:38 -0400

    Progress on about fr now

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -10,8 +10,11 @@ function About() {
       <div className="flex animate-fade items-center justify-center p-1">
         <div className="grid grid-cols-1 place-items-start gap-3 gap-y-5 lg:grid-cols-2 text-white">
           <img src="https://cdn.averwhy.net/IMG_8388.jpg" />
-          <p>short bio here</p>
-          <p>my history in tech</p>
+          <p>
+            My name is Avery. I'm a 20 year old aspiring programmer. I'm from New Hampshire, and im currently studying Computer Science Southern New Hampshire University.
+
+          </p>
+          <p>I've always loved technology. Ever since I first started using the computer at 7 years old, I was drawn to it more than anything. I played Minecraft almost every day on my families iMac.</p>
           <p>another pic of some kind</p>
         </div>
       </div>
